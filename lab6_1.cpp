a0738fa4c0a18a237aeb5600ba0e769c0d8295aa
phuntarikaa <phuntarikaa@gmail.com>
2025-12-09 17:00:44 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int x;
+    int Even=0;
+    int Odd=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    while(x!=0){
+        if(x%2==0){
+            Even++;
+        }
+        else{
+            Odd++;
+        }
+        cout << "Enter an integer: ";
+        cin >> x;
+
+    }
+    cout << "#Even numbers = " << Even<<"\n" ;
+    cout << "#Odd numbers = " << Odd ;
     return 0;
 }
